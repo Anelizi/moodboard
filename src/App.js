11afc91cfbe50a26c173aa1b175330adfb3fa1ee
Tiago <tiagoalex0202@gmail.com>
2023-04-27 14:54:11 -0300

    Desenvolvimento da ProductPage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, useActionData } from "react-router-dom";
 import SignIn from "./pages/SignInPage";
 import SignUp from "./pages/SignUpPage";
 import Home from "./pages/HomePage";
 import Product from "./pages/ProductPage";
 import Cart from "./pages/CartPage";
 import PurchasesMade from "./pages/PurchasesMadePage"
+import { useState } from "react";
+import ProductContext from "./contexts/ProductContext";
 
 export default function App() {
+  const [image, setImage]= useState("")
+  const [product, setProduct]= useState("")
+  const [description, setDescription]= useState("")
+  const [price, setPrice]= useState(0)
+  const [amount, setAmount]= useState(0)
+  const ProductValue= {image: image, setImage: setImage, product: product, setProduct: setProduct, description: description, setDescription: setDescription, price: price, setPrice: setPrice, amount: amount, setAmount: setAmount}
   return (
   <PagesContainer>
     <BrowserRouter>
@@ -14,7 +22,7 @@ export default function App() {
         <Route path="/" element={<SignIn/>} />
         <Route path="/cadastro" element={<SignUp/>} />
         <Route path="/home" element={<Home/>} />
-        <Route path="/product" element={<Product/>} />
+        <Route path="/product/:product_name" element={<ProductContext.Provider value={ProductValue}><Product/></ProductContext.Provider>} />
         <Route path="/cart" element={<Cart/>} />
         <Route path="/purchasesMade" element={<PurchasesMade/>} />
       </Routes>
--- src/pages/ProductPage.js
+++ src/pages/ProductPage.js
@@ -1,5 +1,78 @@
+import styled from "styled-components"
+import axios from "axios"
+import { useNavigate } from "react-router-dom";
+import { useEffect , useParams} from "react"
+import { useContext } from "react";
+import ProductContext from "../contexts/ProductContext";
+
 export default function Product(){
+
+    const {image, setImage, description, setDescription, product, setProduct, price, setPrice, amount, setAmount }= useContext(ProductContext)
+
+    const params = useParams();
+    const navigate= useNavigate();
+
+    useEffect(() => {
+        const requisition= axios.get("mongodb://localhost:27017/moodboard/produtos",{ headers: { Authorization: `Bearer ${auth}`,Product: params.product_name }})
+        requisition. then((response)=>{ setImage(response.data.image);
+        setDescription(response.data.description);
+        setProduct(response.data.productname)
+        setPrice(response.data.price)})
+    },[])
+
+    function AddToCart(event){
+        event.preventDefault()
+        const body= {product: product, amount: amount, price: price }
+        const requisition= axios.post("mongodb://localhost:27017/moodboard/produtos",body);
+        requisition.then(navigate("/cart"))
+        requisition.catch((err)=> alert(err.message))
+    }
     return(
-        <></>
+        <Background>
+            <Header>
+                <Logo>moodboard</Logo>
+                <Icons>
+                  <ion-icon name="cart"></ion-icon>
+                  <ion-icon name="person-circle-outline"></ion-icon>
+                  <ion-icon name="exit-outline"></ion-icon>
+                </Icons>
+            </Header>
+            <PageContent></PageContent>
+        </Background>
     )
 }
+
+const Background= styled.div `
+background-color: white;
+heigth:100%;
+width:100%;`
+
+const Header= styled.header `
+display:flex;
+flex-direction: row;
+width:100%;
+outline-color: gray;
+outline-style: inset;
+margin-bottom:50px;
+`
+
+const Logo= styled.div `
+color: #6cc4b1;
+font-family: Open Sans;
+font-size: x-large;
+font-weigth: 700;
+`
+
+const Icons= styled.div `
+display: flex;
+flex-direction:row;
+justify-content: space-between;`
+
+const PageContent= styled.div `
+display: flex;
+flex-direction:row;
+justify-content: space-between;
+align-items:center;`
+
+const Product= styled.div `
+`
